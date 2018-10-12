e89ee19ec5cd6e6a29530143d458df3349c4c7da
Louis St-Amour <LouisStAmour@gmail.com>
2018-10-12 02:54:46 -0400

    Switching to use new Rails 5 API config

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+# Pick the frameworks you want:
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+# require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
+# require 'action_cable/engine'
+# require 'rails/test_unit/railtie'
+# require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +18,11 @@ Bundler.require(*Rails.groups)
 
 module InterviewProject
   class Application < Rails::Application
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
