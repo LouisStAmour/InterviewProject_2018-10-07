8fd3b89dc86c221b9d7198798d8ee481cb82c64c
Louis St-Amour <LouisStAmour@gmail.com>
2018-10-15 01:48:04 -0400

    Adding only one primary key constraint, also encoding encryption keys as base64.

--- app/models/data_encrypting_key.rb
+++ app/models/data_encrypting_key.rb
@@ -1,9 +1,21 @@
 class DataEncryptingKey < ActiveRecord::Base
 
   attr_encrypted :key,
-                 key: :key_encrypting_key
+                 key: :key_encrypting_key,
+                 encode: true, encode_iv: true
 
   validates :key, presence: true
+  validate :only_one_primary_key
+
+  # TODO: Consider moving this constraint into the database -- or another table or KV store.
+  # https://dba.stackexchange.com/questions/197562/constraint-one-boolean-row-is-true-all-other-rows-false
+  # Alternatively, allow multiple primary keys but make sure the most recent one is chosen.
+  # Or when adding new keys, if primary is set to true, lock the table, then invalidate all other rows?
+  def only_one_primary_key
+    if primary && self.class.where(primary: true).count >= 1
+      errors.add(:primary, "must be false if another primary already exists")
+    end
+  end
 
   def self.primary
     find_by(primary: true)
@@ -15,6 +27,18 @@ class DataEncryptingKey < ActiveRecord::Base
 
   def key_encrypting_key
     ENV['KEY_ENCRYPTING_KEY']
+  end
+
+  def make_primary!
+    transaction do
+      # Uses a non-access lock, so the table can still be read, just not written to during this transaction.
+      ActiveRecord::Base.connection.execute("LOCK #{self.class.table_name} IN EXCLUSIVE MODE")
+      p = self.class.primary
+      p.primary = false
+      p.save!
+      self.primary = true
+      self.save!
+    end
   end
 end
 
--- test/models/data_encrypting_key_test.rb
+++ test/models/data_encrypting_key_test.rb
@@ -11,4 +11,23 @@ class DataEncryptingKeyTest < ActiveSupport::TestCase
       assert key
     end
   end
+
+  test "cannot generate two primary keys" do
+    key = DataEncryptingKey.generate!(primary: true)
+    exception = assert_raises ActiveRecord::RecordInvalid do
+      key2 = DataEncryptingKey.generate!(primary: true)
+    end
+    assert_equal('Validation failed: Primary must be false if another primary already exists', exception.message)
+  end
+
+  test "can set another key as primary" do
+    nonprimary = DataEncryptingKey.generate!(primary: false)
+    primary = DataEncryptingKey.generate!(primary: true)
+    assert_equal DataEncryptingKey.primary, primary
+    nonprimary.make_primary!
+    nonprimary.reload
+    primary.reload
+    assert_equal false, primary.primary, "previous primary key should have primary = false"
+    assert_equal true, nonprimary.primary, "new primary key should have primary = true"
+  end
 end
